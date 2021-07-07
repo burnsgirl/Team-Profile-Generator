ceceb7c26eb8aec57848c6152c3e739f52a2870a
Dauphine Burns <dauphineburns@gmail.com>
2021-07-07 16:24:07 -0400

    HTML try1

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 
 
-const Manager = require("./lib/Manager");
-const Engineer = require("./lib/Engineer");
-const Intern = require("./lib/Intern");
+const Manager = require("../Team-Profile-Generator/lib/Manager");
+const Engineer = require("../Team-Profile-Generator/lib/Engineer");
+const Intern = require("../Team-Profile-Generator/lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
+
 
 const OUTPUT_DIR = path.resolve(__dirname, "output")
 const outputPath = path.join(OUTPUT_DIR, "team.html");
--- src/page-template.js
+++ src/page-template.js
@@ -3,6 +3,7 @@ const Engineer = require("../lib/Engineer");
 const Intern = require("../lib/Intern");
 const Manager = require("../lib/Manager");
 
+// need to have something called teammember
 
 //Create Engineer Card
 const generateEngineer = function(engineer) {
@@ -10,13 +11,13 @@ const generateEngineer = function(engineer) {
     <div class="col-4 mt-4">
         <div class="card h-100">
             <div class="card-header">
-                <h3>${engineer.name}</h3>
+                <h3>${engineer.getName()}</h3>
                 <h4>Engineer</h4><i class="material-icons">laptop_mac</i>
             </div>
             <div class="card-body">
-                <p class="id">ID: ${engineer.id}</p>
-                <p class="email">Email: <a href="mailto:${engineer.email}">${engineer.email}</a></p>
-                <p class="github">Github: <a href="https://github.com/${engineer.github}">${engineer.github}</a></p>
+                <p class="id">ID: ${engineer.getId()}</p>
+                <p class="email">Email: <a href="mailto:${engineer.getEmail()}">${engineer.getEmail()}</a></p>
+                <p class="github">Github: <a href="https://github.com/${engineer.getGithub()}">${engineer.getGithub()}</a></p>
             </div>
         </div>
     </div>
@@ -29,13 +30,13 @@ const generateIntern = function (intern) {
     <div class="col-4 mt-4">
         <div class="card h-100">
             <div class="card-header">
-                <h3>${intern.name}</h3>
+                <h3>${intern.getName()}</h3>
                 <h4>Intern</h4><i class="material-icons">assignment_ind</i>
             </div>
             <div class="card-body">
-                <p class="id">ID: ${intern.id}</p>
-                <p class="email">Email:<a href="mailto:${intern.email}">${intern.email}</a></p>
-                <p class="school">School: ${intern.school}</p>
+                <p class="id">ID: ${intern.getId()}</p>
+                <p class="email">Email:<a href="mailto:${intern.getEmail()}">${intern.getEmail()}</a></p>
+                <p class="school">School: ${intern.getSchool()}</p>
             </div>
         </div>
     </div>
@@ -48,16 +49,94 @@ const generateManager = function (manager) {
     <div class="col-4 mt-4">
         <div class="card h-100">
             <div class="card-header">
-                <h3>${manager.name}</h3>
+                <h3>${manager.getName()}</h3>
                 <h4>Manager</h4><i class="material-icons">content_paste</i>
             </div>
             <div class="card-body">
-                <p class="id">ID: ${manager.id}</p>
-                <p class="email">Email: <a href="mailto:${manager.email}">${manager.email}</a></p>
-                <p class="office">Office Number: ${manager.officeNumber}</p>
+                <p class="id">ID: ${manager.getId()}</p>
+                <p class="email">Email: <a href="mailto:${manager.getEmail()}">${manager.getEmail()}</a></p>
+                <p class="office">Office Number: ${manager.getOfficeNum()}</p>
             </div>
         </div>
     </div>
     `
 };
 
+//Putting The Information On The Page
+generateHTML = (data) => {
+
+//Building Cards
+    pageArray = []; 
+
+    for (let i = 0; i < data.length; i++) {
+        const employee = data[i];
+        const role = employee.getRole(); 
+
+//Generating Engineer
+        if (role === "Engineer") {
+            const engineerCard = generateEngineer(employee);
+
+            pageArray.push(engineerCard);
+        };
+
+//Generating Intern
+        if (role === "Intern") {
+            const internCard = generateIntern(employee);
+
+            pageArray.push(internCard);
+        };
+
+//Generating Manager
+        if (role === "Manager") {
+            const managerCard = generateManager(employee);
+
+            pageArray.push(managerCard);
+        };
+        
+    };
+
+//Joining Different Cards
+    const employeeCards = pageArray.join('');
+
+//Generating Team With Cards
+    const generateTeam = generateTeamPage(employeeCards); 
+    return generateTeam;
+
+};
+
+//Generating Page
+const generateTeamPage = function (employeeCards) {   
+    return`
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Team Profile</title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    </head>
+    <body>
+        <header>
+            <nav class="navbar" id="navbar">
+                <span class="navbar-brand mb-0 h1 w-100 text-center" id="navbar-text">Team Profile</span>
+            </nav>
+        </header>
+        <main>
+            <div class="container">
+                <div class="row justify-content-center" id="team-cards">
+                    <!--Team Cards-->
+                    ${employeeCards}
+                </div>
+            </div>
+        </main>
+        
+    </body>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+    </html>
+  `
+  };
+  
+  //Exporting Information
+  module.exports = generateHTML;
