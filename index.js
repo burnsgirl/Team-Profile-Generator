9b45b670ffba8e4b4b4fdda0ccedbbc2e9f92993
Dauphine Burns <dauphineburns@gmail.com>
2021-07-08 21:54:03 -0400

    renamed index file

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const fs = require("fs");
 
 
 const OUTPUT_DIR = path.resolve(__dirname, "output")
-const outputPath = path.join(OUTPUT_DIR, "team.html");
+const outputPath = path.join(OUTPUT_DIR, "index.html");
 
 const render = require("./src/page-template.js");
 
